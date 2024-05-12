43a649452f44ff4a4d331894fa6b2f020cc18ef0
최주예 <>
2024-05-12 12:01:15 +0900

    Dev_Style/ Fix: Suspense, Lazy를 통해 코드 분할 로직 추가
    - React에서 제공하는 Suspense, Lazy 구문을 통해 route 별 코드 분할 로직 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 // 라이브러리
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// 컴포넌트
-import CalcaulteFrame from "./calcaulte/component/frame/CalculateFrame";
+import React, { Suspense } from "react";
 
 // 스타일
 import GlobalStyle from "./style/GlobalStyle";
 import GlobalTheme from "./style/GlobalTheme";
+
+// 컴포넌트
+const CalcaulteFrame = React.lazy(
+  () => import("./calcaulte/component/frame/CalculateFrame")
+);
 
 /**
  * @desc path별로 컴포넌트를 렌더링하기 위합니다.
@@ -15,20 +18,22 @@ import GlobalTheme from "./style/GlobalTheme";
  */
 function Router(): JSX.Element {
   return (
-    <ThemeProvider theme={GlobalTheme}>
-      <BrowserRouter>
-        {/** 전역 스타일을 적용하기 위합니다. */}
-        <GlobalStyle />
+    <Suspense>
+      <ThemeProvider theme={GlobalTheme}>
+        <BrowserRouter>
+          {/** 전역 스타일을 적용하기 위합니다. */}
+          <GlobalStyle />
 
-        <Routes>
-          {/** calculate 경로로 진입한 경우입니다. */}
-          <Route path="/calculate" element={<CalcaulteFrame />} />
+          <Routes>
+            {/** calculate 경로로 진입한 경우입니다. */}
+            <Route path="/calculate" element={<CalcaulteFrame />} />
 
-          {/** 그 외의 경로로 진입한 경우입니다. */}
-          <Route path="/*" element={<Navigate to="/calculate" />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+            {/** 그 외의 경로로 진입한 경우입니다. */}
+            <Route path="/*" element={<Navigate to="/calculate" />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Suspense>
   );
 }
 
